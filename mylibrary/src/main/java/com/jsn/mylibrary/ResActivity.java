fa498014a7ae4882deb6f37d5fc5748128f9e615
admin <450121066@qq.com>
2021-05-20 20:33:52 +0800

    简化了 activity for result

---
+++ mylibrary/src/main/java/com/jsn/mylibrary/ResActivity.java
@@ -0,0 +1,18 @@
+package com.jsn.mylibrary;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ResActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getFragmentManager().beginTransaction().add()
+    }
+
+  /*  public static void startActivityForRes(Activity activity, Intent intent){
+
+    }*/
+}
---
+++ mylibrary/src/main/java/com/jsn/mylibrary/ResFragment.java
@@ -0,0 +1,69 @@
+package com.jsn.mylibrary;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import android.support.v4.app.FragmentActivity;
+
+public class ResFragment extends Fragment {
+
+    private Intent intent;
+
+    int requestCode;
+
+    public void init(Intent intent,int code,ResultCallback callback){
+        this.intent=intent;
+        this.requestCode=code;
+        this.resultCallback=callback;
+    }
+
+    ResultCallback resultCallback;
+
+    public interface ResultCallback{
+
+        void onActivityResult(int resultCode, @Nullable Intent data);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        startActivityForResult(intent,requestCode);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        resultCallback.onActivityResult(resultCode, data);
+        try {
+            getActivity().getFragmentManager().beginTransaction().remove(this).commitAllowingStateLoss();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        intent=null;
+        resultCallback=null;
+    }
+
+    //it may throw
+    public static void  startActivityForRes(Activity activity,
+                                            Intent intent,
+                                            int requestCode,
+                                            ResultCallback callback){
+        ResFragment fragment=new ResFragment();
+        fragment.init(intent,requestCode,callback);
+        activity.getFragmentManager().beginTransaction().add(fragment,ResFragment.class.getSimpleName()).commit();
+
+    }
+
+
+
+}
