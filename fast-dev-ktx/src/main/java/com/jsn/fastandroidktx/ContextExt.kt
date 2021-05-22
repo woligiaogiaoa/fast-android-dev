50498c4376c1f408839816f573b34ab8af8547a8
admin <450121066@qq.com>
2021-05-22 17:42:13 +0800

    add context extensions

---
+++ fast-dev-ktx/src/main/java/com/jsn/fastandroidktx/ContextExt.kt
@@ -0,0 +1,8 @@
+package com.jsn.fastandroidktx
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.showToast(s:String,duration:Int=Toast.LENGTH_SHORT)=
+        Toast.makeText(this,s,duration).show()
+
